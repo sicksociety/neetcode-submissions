3a096e452d73de72e3bdd85023cfc7786e2218b8
sicksociety <khalouaoui.firas.tn@gmail.com>
2026-08-05 13:27:28 +0100

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<std::string,std::vector<std::string>> sol;
+        for (std::string str : strs){
+            std::string key= str;
+            std::sort(key.begin(),key.end());
+            sol[key].push_back(str);
+        }
+        std::vector<std::vector<std::string>> result;
+        for (auto kv: sol){
+            result.push_back(kv.second);
+        }
+        return (result);
+         
+        
+    }
+};
