a18d2a89f6f9a1c2c330242658765653ef66d871
sicksociety <khalouaoui.firas.tn@gmail.com>
2026-08-14 07:10:55 +0100

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+
+string encode(vector<string>& strs) {
+        string result = "";
+        for (const string& str : strs) {
+            result += to_string(str.length()) + "#" + str;
+        }
+        return result;
+    }
+
+    vector<string> decode(string s) {
+        vector<string> result;
+        int i = 0;
+        while (i < s.length()) {
+            int j = i;
+            while (s[j] != '#') {
+                j++;
+            }
+            int length = stoi(s.substr(i, j - i));
+            string str = s.substr(j + 1, length);
+            result.push_back(str);
+            i = j + 1 + length;
+        }
+        return result;
+    }
+
+};
