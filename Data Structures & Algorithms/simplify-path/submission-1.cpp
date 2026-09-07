cbe03c112b30f22c8873b11438c21b4d8033a6a2
sicksociety <khalouaoui.firas.tn@gmail.com>
2026-09-07 08:53:44 +0100

    Add: simplify-path - submission-1

---
+++ Data Structures & Algorithms/simplify-path/submission-1.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        stringstream ss(path);
+        stack<string> sol;
+        string token;
+        while (std::getline(ss,token,'/')){
+            if ( token==".."){
+                if (sol.empty()){
+                    continue;
+                }
+                else{
+                    sol.pop();
+
+                }
+            }
+            else if (token.empty() || token=="." ){
+                continue;
+            }
+            else{
+                sol.push(token);
+            }
+       }
+       if (sol.empty()){
+        return "/";
+       }
+       string solution="";
+       while (!sol.empty()){
+        solution="/"+sol.top()+solution;
+        sol.pop();
+        
+       }
+       return solution;
+
+
+        
+    }
+};
